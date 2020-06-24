9e1f28506779c63b7d6e39e07e18b1d598a76164
hyigor <48020648+hyigor@users.noreply.github.com>
2020-06-24 17:28:19 -0300

    segundo commit

--- prog-sis2/src/main/java/br/mack/ps2/api/Economia.java
+++ prog-sis2/src/main/java/br/mack/ps2/api/Economia.java
@@ -1,4 +1,52 @@
 package br.mack.ps2.api;
 
+
+import java.util.Date;
+
 public class Economia {
+    private int id;
+    private Date date;
+    private int value;
+
+    public Economia(){
+        this.id = -1;
+        this.date = null;
+        this.value = 0;
+    }
+
+    public Economia(int id, Date date, int value) {
+        this.id = id;
+        this.date = date;
+        this.value = value;
+    }
+
+    public int getId(){ return id;}
+
+    public void setId(int id){this.id = id;}
+
+    public Date getDate() {
+        return date;
+    }
+
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Economia{" +
+                "id=" + id +
+                ", date=" + date +
+                ", value=" + value +
+                '}';
+    }
 }
--- prog-sis2/src/main/java/br/mack/ps2/api/Result.java
+++ prog-sis2/src/main/java/br/mack/ps2/api/Result.java
@@ -1,4 +1,37 @@
 package br.mack.ps2.api;
 
-public class Result {
+import java.io.Serializable;
+
+public class Result implements Serializable {
+    int id;
+    String date;
+    int value;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+
+
+
 }
