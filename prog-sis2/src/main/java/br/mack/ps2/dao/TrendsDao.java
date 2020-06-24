a2c57c2a7003c2d5b13de310d45cecc09dd4c7a1
arthurgusmaodematheus <61274456+arthurgusmaodematheus@users.noreply.github.com>
2020-06-24 18:24:35 -0300

    Setimo commit

--- prog-sis2/src/main/java/br/mack/ps2/dao/TrendsDao.java
+++ prog-sis2/src/main/java/br/mack/ps2/dao/TrendsDao.java
@@ -1,4 +1,17 @@
 package br.mack.ps2.dao;
 
+import br.mack.ps2.api.Economia;
+import br.mack.ps2.api.Result;
+import br.mack.ps2.api.Trends;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
 public class TrendsDao {
-}
+    Trends dataBase;
+    EconomiaDao economiaDAO;
+
+    public TrendsDao(EconomiaDao economiaDAO) {
+        this.economiaDAO = economiaDAO;
+        this.dataBase = new Trends();
+        read();
+    }
