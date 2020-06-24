484a57d341cf5f171210eccbeddf638a39df4ed9
hyigor <48020648+hyigor@users.noreply.github.com>
2020-06-24 18:28:12 -0300

    Oitavo commit

--- prog-sis2/src/main/java/br/mack/ps2/dao/TrendsDao.java
+++ prog-sis2/src/main/java/br/mack/ps2/dao/TrendsDao.java
@@ -15,3 +15,40 @@ public class TrendsDao {
         this.dataBase = new Trends();
         read();
     }
+
+
+    public void read() {
+
+        List<Economia> economias = economiaDAO.read();
+        List<Economia> economias2 = economias;
+        for (int i = 0; i < economias.size(); i++) {
+
+            SimpleDateFormat in = new SimpleDateFormat("yyyy-MM-dd");
+            Economia economia = economias.get(i);
+
+
+            Result result = new Result();
+            result.setDate(in.format(economia.getDate()));
+            result.setValue(economia.getValue());
+            this.dataBase.getResults().add(result);
+        }
+
+
+    }
+
+    public Trends getAllTrends() {
+        return this.dataBase;
+    }
+
+
+    public void updateResult(Result result) {
+
+        for (int i = 0; i < this.dataBase.getResults().size(); i++) {
+            if (this.dataBase.getResults().get(i).getDate().equals(result.getDate())) {
+                this.dataBase.getResults().set(i, result); //fixme: no delete é só trocar esse set por remove
+            }
+        }
+
+
+    }
+}
