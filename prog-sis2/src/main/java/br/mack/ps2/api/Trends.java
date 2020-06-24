c4471e61360d8da76035ddce677b38424e34fa43
arthurgusmaodematheus <61274456+arthurgusmaodematheus@users.noreply.github.com>
2020-06-24 17:52:49 -0300

    terceiro commit

--- prog-sis2/src/main/java/br/mack/ps2/api/Trends.java
+++ prog-sis2/src/main/java/br/mack/ps2/api/Trends.java
@@ -1,4 +1,38 @@
 package br.mack.ps2.api;
 
-public class Trends {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Trends implements Serializable {
+    private String term = "Coronavirus";
+    private List<Result> results;
+
+    public Trends() {
+        this.results = new ArrayList();
+    }
+
+    public Trends(String term, List<Result> data) {
+        this.term = term;
+        this.results = data;
+    }
+
+    public String getTerm() {
+        return term;
+    }
+
+    public void setTerm(String term) {
+        this.term = term;
+    }
+
+    public List<Result> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Result> results) {
+        this.results = results == null ? new ArrayList() : results;
+    }
+
+    {
+    }
 }
--- prog-sis2/src/main/java/br/mack/ps2/dao/EconomiaDao.java
+++ prog-sis2/src/main/java/br/mack/ps2/dao/EconomiaDao.java
@@ -1,4 +1,14 @@
 package br.mack.ps2.dao;
 
+import br.mack.ps2.api.Economia;
+
+import java.util.List;
+
 public interface EconomiaDao {
+    boolean create(Economia economia);
+    List<Economia> read();
+    boolean update(Economia economia);
+    boolean delete(Economia economia);
+
+
 }
